51d25c0979fd9c8dd97e1f8436f545e68755d0c1
KimMead <kimmeadows8@gmail.com>
2020-09-21 20:18:10 -0500

    Add migrations and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+Quote.destroy_all
+
+motivational = Category.create(name: "Motivational")
+funny = Category.create(name: "Funny")
+friend = Category.create(name: "Friend")
+
+Quote.create(content: "Hello!", category_id: motivational.id)
